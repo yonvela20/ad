29e1bf465d774a28e703222a1fa64072f60761ec
yonvela20 <yon.vela20@gmail.com>
2018-01-24 12:38:10 +0100

    Añade un nuevo articulo en la base de datos

--- GVenta/src/main/java/serpis/ad/PruebaVenta.java
+++ GVenta/src/main/java/serpis/ad/PruebaVenta.java
@@ -1,15 +1,14 @@
 package serpis.ad;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
 import org.jboss.logging.LoggerProvider;
 
 public class PruebaVenta {
@@ -29,6 +28,7 @@ public class PruebaVenta {
 		showCategoria();
 		showCliente();
 		showPedido();
+		newArticulo();
 		//showPedidoLinea();
 		
 		entityManagerFactory.close();
@@ -37,7 +37,9 @@ public class PruebaVenta {
 	private static void showArticulo() {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
+		
 		List<Articulo> articulos = entityManager.createQuery("from Articulo order by id", Articulo.class).getResultList();
+		
 		for (Articulo articulo : articulos)
 			System.out.println(articulo);
 		entityManager.getTransaction().commit();
@@ -55,7 +57,9 @@ public class PruebaVenta {
 	public static void showCliente() {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
+		
 		List<Cliente> clientes = entityManager.createQuery("from Cliente order by id", Cliente.class).getResultList();
+		
 		for (Cliente cliente : clientes)
 			System.out.println(cliente);
 		entityManager.getTransaction().commit();
@@ -64,7 +68,9 @@ public class PruebaVenta {
 	public static void showPedido() {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
+		
 		List<Pedido> pedidos = entityManager.createQuery("from Pedido order by id", Pedido.class).getResultList();
+		
 		for(Pedido pedido : pedidos) {
 			System.out.println(pedido);
 		entityManager.getTransaction().commit();
@@ -79,9 +85,22 @@ public class PruebaVenta {
 //			System.out.println(pedidolinea);
 //		entityManager.getTransaction().commit();
 //	}
-	
-	
-	
+		
+	private static void newArticulo() {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		Categoria categoria = entityManager.getReference(Categoria.class, 1L);
+		
+		Articulo articulo = new Articulo();
+		
+		articulo.setNombre("nuevo " + new Date());
+		articulo.setPrecio(new BigDecimal(Math.random()));
+		articulo.setCategoria(categoria);
+		entityManager.persist(articulo);
+		
+		entityManager.getTransaction().commit();		
+	}
+
 }
 	
 
