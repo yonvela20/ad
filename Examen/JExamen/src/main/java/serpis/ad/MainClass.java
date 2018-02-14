84b3879e113b75f54ebf68ae4da0c76a2b4ad399
yonvela20 <yon.vela20@gmail.com>
2018-02-14 12:12:50 +0100

    Hecho el JExamen

--- Examen/JExamen/src/main/java/serpis/ad/MainClass.java
+++ Examen/JExamen/src/main/java/serpis/ad/MainClass.java
@@ -1,9 +1,16 @@
 package serpis.ad;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 //TODO completar
 public class MainClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
 		Categoria categoria = load(1L); 
 		categoria.setNombre(categoria.getNombre() + "#j");
@@ -11,11 +18,33 @@ public class MainClass {
 		
 	}
 	
-	private static Categoria load(Object id) {
-		return null;
+	private static Categoria load(Object id) throws SQLException {
+		Categoria categoria = new Categoria();
+		
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dbprueba","root","sistemas");
+		Statement stmt = connection.createStatement();
+		
+		String sql = "SELECT * FROM categoria WHERE id = 1L";
+		ResultSet rs = stmt.executeQuery(sql);
+				
+		categoria.setId(rs.getLong("id"));
+		categoria.setNombre(rs.getString("nombre"));
+	    
+		connection.close();
+		return categoria;
 	}
 	
-	private static void update(Categoria categoria) {
+	private static void update(Categoria categoria) throws SQLException{
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dbprueba","root","sistemas");
+
+		PreparedStatement pstmt = connection.prepareStatement("UPDATE articulo SET nombre = ? WHERE id = ?");
+		
+		pstmt.setString(1, categoria.getNombre());
+		pstmt.setLong(2, categoria.getId());
+		
+		pstmt.executeUpdate();
+	
+		connection.close();
 	}
 
 }
